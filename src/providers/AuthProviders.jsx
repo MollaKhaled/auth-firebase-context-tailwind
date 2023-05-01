4e84b6d2e278a7eef0db6746645267028731b4bf
Mollakhaled <khaledbalok@gmail.com>
2023-05-01 22:00:55 +0600

    private route and navigate after login

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -10,16 +10,37 @@ const AuthProviders = ({children}) => {
 
 const [user, setUser] = useState(null);
 
+const [loading, setLoading] = useState(true);
+
 const createUser = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password)
 }
 const signIn =(email, password) => {
   return signInWithEmailAndPassword(auth, email, password)
 }
+const logOut = () =>{
+   return signOut(auth);
+}
+
+// Observe auth state change
+useEffect(()=>{
+ const unsubscribe =  onAuthStateChanged(auth, currentUser =>{
+   console.log('auth state change', currentUser);
+   setUser(currentUser);
+   setLoading(false);
+ });
+  return () =>{
+    unsubscribe();
+  }
+},[])
+
 const authInfo ={
   user,
+  loading,
   createUser,
-  signIn
+  signIn,
+  logOut,
+  
 }
   return (
     <div>
