6149f34e3eca1fbfbf2b8a7ee85f4ab30e01d9f7
Mollakhaled <khaledbalok@gmail.com>
2023-05-01 19:29:53 +0600

    create user and sign in user using context api

--- src/__steps__.js
+++ src/__steps__.js
@@ -14,3 +14,9 @@
  * 2.inside provider access children props and then use
  * 
  * */ 
+
+/**
+ * 1.Break down the create user function
+ * 2.Auth provider will have auth and pass email and password from register.
+ * 
+ * ***/ 
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,14 +1,33 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
+export const AuthContext = createContext(null);
 
- export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
-const AuthProviders = ({ children }) => {
-  const user = { displayName: 'Sagor Nodi' }
+const AuthProviders = ({children}) => {
+
+const [user, setUser] = useState(null);
+
+const createUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+const signIn =(email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+}
+const authInfo ={
+  user,
+  createUser,
+  signIn
+}
   return (
-    <AuthContext.Provider value={user}>
-      {children}
-    </AuthContext.Provider>
+    <div>
+      <AuthContext.Provider value={authInfo}>
+         {children}
+      </AuthContext.Provider>
+
+    </div>
   );
 };
 
